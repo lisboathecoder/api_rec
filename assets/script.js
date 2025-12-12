a5b8ceb9cc3ac5012e7efcdc08931149390e9da9
lisboathecoder <gustavo.lisboa2009@gmail.com>
2025-12-12 14:46:04 -0300

    finalizado js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 const img = document.createElement('img');
+const p = document.createElement('p');
+
 document.getElementById('imagem').appendChild(img);
+document.getElementById('imagemId').appendChild(img);
+document.getElementById('nameId').appendChild(p);
 
 async function buscarPersonagem() {
     const input = document.getElementById('nome').value.toLowerCase();
@@ -9,10 +13,32 @@ async function buscarPersonagem() {
         
         if (data.data && data.data.length > 0) {
             img.src = data.data[0].imageUrl;
+            p.textContent = data.data.name
         } else {
             console.log('Personagem não encontrado');
+            alert('Personagem não encontrado. Por favor, tente novamente.');
         }
     } catch (error) {
         console.error('Erro ao buscar o/a Personagem:', error);
+        alert('Erro ao buscar o personagem. Por favor, tente novamente.');
+    }
+}
+
+async function buscarPersonagemId() {
+    const input = document.getElementById('id').value;
+    try {
+        const response = await fetch (`https://api.disneyapi.dev/character/${input}`);
+        const data = await response.json();
+
+        if (data.data) {
+            img.src = data.data.imageUrl;
+            p.textContent = data.data.name;
+        } else {
+            console.log('Personagem não encontrado');
+            alert('Personagem não encontrado. Por favor, tente novamente.');
+        }
+    } catch (error) {
+        console.error('Erro ao buscar o/a Personagem pelo ID:', error);
+        alert('ID inválido. Por favor, tente novamente.');
     }
 }
